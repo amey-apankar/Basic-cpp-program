1a7574eaa204dec36e934adc9b348756f91e8646
Amey Apankar <apankaramey5@gmail.com>
2025-07-31 09:55:18 +0400

    Create input_output.cpp

---
+++ 02-input-output/input_output.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+main(){
+char name[30];
+int age;
+cout<<"Enter your name";
+cin<<name;
+cout<<"Enter your age";
+cin<<age;
+cout<<"Hello,"<<name<<endl;
+cout<<"You are"<<age<<" years old."<<endl;
+getch();
+}
